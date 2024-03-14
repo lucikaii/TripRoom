b66179ff58df5d392862d3dd382269c78eb2e61d
lucikaii <bragaluciano.nan@gmail.com>
2024-03-14 12:19:31 -0300

    pronto

--- app/src/main/java/br/senai/sp/jandira/triproom/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/triproom/MainActivity.kt
@@ -3,7 +3,9 @@ package br.senai.sp.jandira.triproom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,6 +19,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -24,6 +29,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -165,7 +171,10 @@ fun TripRoomLogin(){
                 onClick = { /*TODO*/ },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xff7d8cc4))) {
 
-                Text(text = "SIGN IN")
+                Text(text = "SIGN IN", color = Color(0xffffffff))
+                Icon(
+                    imageVector = Icons.Outlined.KeyboardArrowRight,
+                    contentDescription = "Seta")
             }
         }
 
@@ -231,13 +240,14 @@ fun TripRoomSignup(){
                     .background(
                         color = Color(0xff7d8cc4),
                         shape = RoundedCornerShape(bottomStart = 16.dp)
-                    ))
+                    )
+            )
         }
 
         Column (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 50.dp),
+                .padding(top = 30.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Text(
@@ -264,21 +274,29 @@ fun TripRoomSignup(){
                 modifier = Modifier
                     .height(100.dp)
                     .width(100.dp),
-                shape = CircleShape
+                shape = CircleShape,
+                border = BorderStroke(width = 2.dp, color = Color(0xff7d8cc4))
             ){
-
+                Icon(
+                    imageVector = Icons.Outlined.Person,
+                    contentDescription = "foto de perfil",
+                    tint = Color(0xff7d8cc4),
+                    modifier = Modifier
+                        .height(80.dp)
+                        .width(80.dp)
+                        .padding(start = 17.dp, top = 17.dp)
+                )
             }
         }
 
         Column (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 50.dp)
-                .scrollable(orientation = )
+                .padding(start = 16.dp, end = 16.dp, top = 25.dp)
         ){
 
             OutlinedTextField(
-                value = "LUANA",
+                value = "Kainan Braga",
                 onValueChange = {},
                 label = {
                     Text(text = "Username", color = Color(0xff000000))
@@ -293,14 +311,14 @@ fun TripRoomSignup(){
             )
 
             OutlinedTextField(
-                value = "LUANA",
+                value = "kainan.braga@gmail.com",
                 onValueChange = {},
                 label = {
-                    Text(text = "Username", color = Color(0xff000000))
+                    Text(text = "Email", color = Color(0xff000000))
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 30.dp),
+                    .padding(top = 15.dp),
                 shape = RoundedCornerShape(16.dp),
                 colors = OutlinedTextFieldDefaults.colors(
                     unfocusedBorderColor = Color(0xff7d8cc4),
@@ -309,14 +327,14 @@ fun TripRoomSignup(){
             )
 
             OutlinedTextField(
-                value = "LUANA",
+                value = "shiu123",
                 onValueChange = {},
                 label = {
-                    Text(text = "Username", color = Color(0xff000000))
+                    Text(text = "Password", color = Color(0xff000000))
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 30.dp),
+                    .padding(top = 15.dp),
                 shape = RoundedCornerShape(16.dp),
                 colors = OutlinedTextFieldDefaults.colors(
                     unfocusedBorderColor = Color(0xff7d8cc4),
@@ -325,14 +343,14 @@ fun TripRoomSignup(){
             )
 
             OutlinedTextField(
-                value = "LUANA",
+                value = "shiu123",
                 onValueChange = {},
                 label = {
-                    Text(text = "Username", color = Color(0xff000000))
+                    Text(text = "Confirm Password", color = Color(0xff000000))
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 30.dp),
+                    .padding(top = 15.dp),
                 shape = RoundedCornerShape(16.dp),
                 colors = OutlinedTextFieldDefaults.colors(
                     unfocusedBorderColor = Color(0xff7d8cc4),
@@ -345,15 +363,15 @@ fun TripRoomSignup(){
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 20.dp)
+                .padding(top = 5.dp)
         ){
             Checkbox(
-                checked = false,
+                checked = true,
                 onCheckedChange = {},
                 colors = CheckboxDefaults.colors(
                     checkedColor = Color(0xff7d8cc4),
                     uncheckedColor = Color(0xff7d8cc4),
-                    checkmarkColor = Color(0xff7d8cc4)
+                    checkmarkColor = Color(0xffffffff)
                 ))
             Text(text = "Over 18?")
         }
@@ -361,7 +379,7 @@ fun TripRoomSignup(){
         Row (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 25.dp)
+                .padding(start = 16.dp, end = 16.dp, top = 15.dp)
         ){
             Button(
                 onClick = { /*TODO*/ },
@@ -376,8 +394,51 @@ fun TripRoomSignup(){
                 Text(text = "CREATE ACCOUNT")
             }
         }
+
+        Row (
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Text(
+                text = "Already have an account?",
+                fontSize = 12.sp,
+                color = Color(0xffA09C9C)
+            )
+            Text(text = "Sign in",
+                fontSize = 12.sp,
+                color = Color(0xff7d8cc4),
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.Bottom
+        ){
+
+            Box(
+                modifier = Modifier
+                    .height(40.dp)
+                    .width(120.dp)
+                    .background(
+                        color = Color(0xff7d8cc4),
+                        shape = RoundedCornerShape(topEnd = 16.dp)
+                    )
+            )
+        }
+
+
+
+
+
+
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
